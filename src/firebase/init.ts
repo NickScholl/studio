26a3aaf239032b327f2c9b8ad83df3c32177600e
nickk <68135932+NickScholl@users.noreply.github.com>
2026-05-06 01:42:43 -0300

    Update init.ts

--- src/firebase/init.ts
+++ src/firebase/init.ts
@@ -1,20 +1,13 @@
-import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
-import { getAuth, Auth } from 'firebase/auth';
-import { getFirestore, Firestore } from 'firebase/firestore';
-import { firebaseConfig ) from 'firebase/config';
+import { initializeApp, getApps, getApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+};
 
-export function getFirebaseApp(): FirebaseApp {
-  if (typeof window === 'undefined') {
-    throw new Error('Firebase should only run on client');
-  }
-  return getApps().length ? getApp() : initializeApp(firebaseConfig);
-}
-
-export function getFirebaseAuth(app: FirebaseApp): Auth {
-  return getAuth(app);
-}
-
-export function getFirebaseDb(app: FirebaseApp): Firestore {
-  return getFirestore(app);
-}
+export const app =
+  getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
