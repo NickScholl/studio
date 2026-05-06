46675794660c12a981136dfc4f2f5b0768951164
nickk <68135932+NickScholl@users.noreply.github.com>
2026-05-06 01:36:42 -0300

    Create config.ts

---
+++ src/firebase/config.ts
@@ -0,0 +1,9 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCsHOFhFQj_FdzuFHcDwOL2eaG_nM693ck",
+  authDomain: "studio-5240103926-3ba8a.firebaseapp.com",
+  projectId: "studio-5240103926-3ba8a",
+  storageBucket: "studio-5240103926-3ba8a.firebasestorage.app",
+  messagingSenderId: "866820768040",
+  appId: "1:866820768040:web:79f1b971680fde53b4501d"
+};
