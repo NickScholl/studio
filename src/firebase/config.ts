30af05018080a0e898d6f09a107f02c440915f81
nickk <68135932+NickScholl@users.noreply.github.com>
2026-05-06 01:33:04 -0300

    Delete src/firebase/config.ts

--- src/firebase/config.ts
+++
@@ -1,9 +0,0 @@
-
-export const firebaseConfig = {
-  "projectId": "studio-5240103926-3ba8a",
-  "appId": "1:866820768040:web:79f1b971680fde53b4501d",
-  "apiKey": "AIzaSyCsHOFhFQj_FdzuFHcDwOL2eaG_nM693ck",
-  "authDomain": "studio-5240103926-3ba8a.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "866820768040"
-};
